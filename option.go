a8102a0ca70fbd48ddd5819791d7a195834f12e6
Stev Witzel <switzel@pivotal.io>
2016-10-26 11:17:21 +0100

    Add descriptions for client options and defaults

--- option.go
+++ option.go
@@ -12,10 +12,17 @@ import (
 )
 
 var (
+	// DefaultRetrySelector defines the default selector to be used when selecting
+	// endpoints for request retries.
 	DefaultRetrySelector retry.Selector = retry.RoundRobin
-	DefaultRetryLimit    retry.Allow    = retry.MaxRetries(3)
-	DefaultRetryDelay    retry.Delay    = retry.ConstantDelay(1 * time.Second)
 
+	// DefaultRetryLimit defines the default allowance for request retries.
+	DefaultRetryLimit retry.Allow = retry.MaxRetries(3)
+
+	// DefaultRetryDelay defines the default delay in-between request retries.
+	DefaultRetryDelay retry.Delay = retry.ConstantDelay(1 * time.Second)
+
+	// DefaultTransport defines the default roundtripper used by the internal http client.
 	DefaultTransport = &http.Transport{
 		Dial: (&net.Dialer{
 			Timeout:   5 * time.Second,
@@ -27,9 +34,11 @@ var (
 		MaxIdleConnsPerHost:   1,
 	}
 
+	// DefaultTimeout defines the default timeout used by the internal http client.
 	DefaultTimeout = 10 * time.Second
 )
 
+// Option can be used to configure a Client.
 type Option func(*Client)
 
 // HTTPTimeout sets the timeout for the internal HTTP client.
@@ -66,18 +75,22 @@ func Oauth2ClientCredentials(clientID, clientSecret, tokenURI string, scopes ...
 	}
 }
 
+// RetryLimit instructs the client to limit retries to a given allowance.
 func RetryLimit(limit retry.Allow) Option {
 	return func(c *Client) {
 		c.retryLimit = limit
 	}
 }
 
+// RetrySelector instructs the client to use a given selector to pick endpoints
+// for retries.
 func RetrySelector(selector retry.Selector) Option {
 	return func(c *Client) {
 		c.retrySelector = selector
 	}
 }
 
+// RetryDelay sets the delau the client in-between request retries.
 func RetryDelay(delay retry.Delay) Option {
 	return func(c *Client) {
 		c.retryDelay = delay
